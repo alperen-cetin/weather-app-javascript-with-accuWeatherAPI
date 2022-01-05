4b6cc9ff6dc2c35ad90d9d54d08f9ec2806e4c97
alperen-cetin <79174989+alperen-cetin@users.noreply.github.com>
2022-01-05 04:45:14 +0300

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,61 @@
+const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
+
+const updateUI = (data) => {
+
+    // const cityDets = data.cityDets;
+    // const weather = data.weather;
+
+    //destructure properties
+    const { cityDets, weather} = data;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Metric.Value}</span>
+            <span>&deg;C</span>
+    `;
+    // update day and night / icon images
+    const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+    icon.setAttribute('src', iconSrc);
+
+    let timeSrc = null;
+    if(weather.IsDayTime){
+        timeSrc = 'img/day.jpg';
+    } else {
+        timeSrc = 'img/night.jpg';
+    }
+
+    time.setAttribute('src', timeSrc)
+
+    //remove d-none
+    if(card.classList.contains('d-none')){
+        card.classList.remove('d-none');
+    }
+};
+
+const updateCity = async (city) => {
+
+    const cityDets = await getCity(city);
+    const weather = await getWeather(cityDets.Key);
+
+    return {cityDets, weather};
+};
+
+cityForm.addEventListener('submit', e => {
+    e.preventDefault();
+    //get city value
+    const city = cityForm.city.value.trim();
+    cityForm.reset();
+    //update ui with new city
+    updateCity(city)
+        .then(data => updateUI(data))
+        .catch(err => console.log(err));
+});
+
+const result = true ? 'value 1' : 'value 2';
